40210c9d7da99daf9c9d0d12ad3547b0c16eeb5b
james liu london <jamesliulondon@gmail.com>
2020-07-17 23:21:43 +0100

    okhttp initial

--- 01-okhttp/app/src/main/java/com/example/okhttp/MainActivity.java
+++ 01-okhttp/app/src/main/java/com/example/okhttp/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.okhttp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtString;
+    public String url= "https://www.google.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtString= (TextView)findViewById(R.id.text_view_result);
+
+        try {
+            run();
+
+        } catch (IOException e) {
+            txtString.setText(e.toString());
+            e.printStackTrace();
+
+        }
     }
+
+    void run() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtString.setText(myResponse);
+                    }
+                });
+
+            }
+        });
+    }
+
 }
